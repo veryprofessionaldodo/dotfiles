f9ebc5066a340895ce0d07b57c906f314804da15
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-11-01 10:37:45 +0000

    Added automatic Setup

--- desktop-setup.sh
+++ desktop-setup.sh
@@ -1,5 +1,7 @@
  #!/bin/bash
 
+cd Desktop
+
 cp config ~/.i3/config
 
 cp polybar/config ~/.config/polybar/config
--- laptop-setup.sh
+++ laptop-setup.sh
@@ -1,5 +1,7 @@
  #!/bin/bash
 
+cd Laptop
+
 cp config ~/.i3/config
 
 cp polybar/config ~/.config/polybar/config
