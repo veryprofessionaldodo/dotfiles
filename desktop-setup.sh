d868382ee5fdb89fbf873041a18fb2f23eb31c4e
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-11-01 12:03:32 +0000

    Updated automated process

--- desktop-setup.sh
+++ desktop-setup.sh
@@ -1,14 +1,17 @@
  #!/bin/bash
 
+cp .Xresources ~/.Xresources
+
+cp compton.conf ~/.config/compton.conf
+
 cd Desktop
 
 cp config ~/.i3/config
 
 cp polybar/config ~/.config/polybar/config
 
-cd ..
 
-cp .Xresources ~/.Xresources
+
 
 
 
--- laptop-setup.sh
+++ laptop-setup.sh
@@ -1,14 +1,15 @@
  #!/bin/bash
 
+cp .Xresources ~/.Xresources
+
+cp compton.conf ~/.config/compton.conf
+
 cd Laptop
 
 cp config ~/.i3/config
 
 cp polybar/config ~/.config/polybar/config
 
-cd ..
-
-cp .Xresources ~/.Xresources
 
 
 
