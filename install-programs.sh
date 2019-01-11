96d98821c8f59c9fab7f8bae6bff9970139adc92
Pedro Daniel Reis <up201506046@fe.up.pt>
2019-01-11 18:49:53 +0000

    Added startup script to install apps

---
+++ install-programs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+yaourt -S franz openssl openssl-1.0 firefox code spotify discord variety unzip sl rpm-org rescuetime nautilus libtiff krita i3lock-color gnome-system-monitor gnome-screenshot gimp gamin coreutils
+
