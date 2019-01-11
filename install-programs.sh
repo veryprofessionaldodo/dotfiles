a99e49d1723644f3763392619072db0bc18cf572
workingdodo <pedroreis.ad@gmail.com>
2019-01-11 21:00:30 +0000

    Added simplenote

--- install-programs.sh
+++ install-programs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz openssl-1.0 firefox code spotify discord variety sl rpm-org rescuetime 
-nautilus krita i3lock-color gnome-system-monitor gnome-screenshot gimps gitkraken rofi polybar
+yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz openssl-1.0 firefox code spotify discord variety sl rpm-org rescuetime nautilus krita i3lock-color gnome-system-monitor gnome-screenshot gimps gitkraken rofi polybar gedit redshift libpng12 audiofile e2fsprogs libjpeg6-turbo libxp tcsh xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-misc simplenote-electron-bin
 
