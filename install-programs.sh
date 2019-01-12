1fdf2a2d3509aaaee210df7c8fe2407f39f6b860
workingdodo <pedroreis.ad@gmail.com>
2019-01-12 11:23:39 +0000

    Added libreoffice

--- install-programs.sh
+++ install-programs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz openssl-1.0 firefox code spotify discord variety sl rpm-org rescuetime nautilus krita i3lock-color gnome-system-monitor gnome-screenshot gimps gitkraken rofi polybar gedit redshift libpng12 audiofile e2fsprogs libjpeg6-turbo libxp tcsh xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-misc simplenote-electron-bin deluge
+yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz openssl-1.0 firefox code spotify discord variety sl rpm-org rescuetime nautilus krita i3lock-color gnome-system-monitor gnome-screenshot gimps gitkraken rofi polybar gedit redshift libpng12 audiofile e2fsprogs libjpeg6-turbo libxp tcsh xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-misc simplenote-electron-bin deluge libreoffice-fresh
 
