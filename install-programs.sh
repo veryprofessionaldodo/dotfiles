d83d7c3bb00698b729aa08e9ea5715963c8898cb
workingdodo <pedroreis.ad@gmail.com>
2019-03-01 16:39:46 +0000

    Added folder

--- install-programs.sh
+++ install-programs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz firefox-nightly code spotify discord variety sl rpm-org rescuetime nautilus krita i3lock-color gnome-system-monitor gnome-screenshot gimp rofi polybar gedit redshift simplenote-electron-bin deluge libreoffice-fresh xf86-input-wacom digimend-kernel-drivers-dkms-git gsimplecal lmms simplenote-electron-bin
+yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz firefox-nightly code spotify variety sl rpm-org rescuetime nautilus krita gnome-system-monitor gnome-screenshot gimp rofi polybar gedit redshift simplenote-electron-bin deluge libreoffice-fresh digimend-kernel-drivers-dkms-git gsimplecal lmms simplenote-electron-bin betterlockscreen blender-2.8-bin osx-arc-darker gtk-chtheme
