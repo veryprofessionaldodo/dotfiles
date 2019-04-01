051f0fda17edb7fdc3907e8d9185c9a9f71b3590
workingdodo <pedroreis.ad@gmail.com>
2019-04-01 18:19:30 +0100

    Added new wallpw wallpaper

--- install-programs.sh
+++ install-programs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz firefox-nightly code spotify variety sl rpm-org rescuetime2 nautilus krita gnome-system-monitor gnome-screenshot gimp rofi polybar gedit redshift simplenote-electron-bin deluge libreoffice-fresh digimend-kernel-drivers-dkms-git gsimplecal lmms simplenote-electron-bin betterlockscreen blender-2.8-bin osx-arc-darker gtk-chtheme steam-manjaro
+yaourt --m-arg "--skipchecksums --skippgpcheck" -S franz firefox-nightly code spotify variety sl rpm-org rescuetime2 nautilus krita gnome-system-monitor gnome-screenshot gimp rofi polybar gedit redshift simplenote-electron-bin deluge libreoffice-fresh digimend-kernel-drivers-dkms-git gsimplecal lmms simplenote-electron-bin betterlockscreen blender-2.8-bin osx-arc-darker gtk-chtheme steam-manjaro compton-tryone-git
