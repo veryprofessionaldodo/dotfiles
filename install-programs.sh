ee7c8b4a905742a33f474948efa0f18b22ccd003
veryprofessionaldodo <pedroreis.ad@protonmail.com>
2019-04-22 13:18:35 +0100

    Added alsa utils

--- install-programs.sh
+++ install-programs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S file-roller feh franz firefox code spotify variety sl rpm-org rescuetime2 krita gimp rofi polybar redshift simplenote-electron-bin deluge libreoffice-fresh gsimplecal lmms simplenote-electron-bin betterlockscreen osx-arc-darker gtk-chtheme compton-tryone-git
+yay -S alsa-utils file-roller feh franz firefox code spotify variety sl rpm-org rescuetime2 krita gimp rofi polybar redshift simplenote-electron-bin deluge libreoffice-fresh gsimplecal lmms simplenote-electron-bin betterlockscreen osx-arc-darker gtk-chtheme compton-tryone-git
