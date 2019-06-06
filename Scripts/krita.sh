ca569fb9ccb67dac2dd0d1bdd24e14172b111414
veryprofessionaldodo <pedroreis.ad@protonmail.com>
2019-06-06 19:46:32 +0100

    Added SimpleScreenRecorder with Krita

---
+++ Scripts/krita.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec simplescreenrecorder & krita 
--- scripts.sh
+++ scripts.sh
@@ -2,6 +2,8 @@
 
 cd Scripts 
 
+sudo cp org.kde.krita.desktop /usr/share/applications/
+
 sudo cp soundcloud.desktop /usr/share/applications/
 
 sudo cp gaomon_2D.desktop /usr/share/applications/
