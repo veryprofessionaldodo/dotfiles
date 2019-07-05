8748888cd1d5cd1745ec13c002ddd976391e945b
veryprofessionaldodo <pedroreis.ad@protonmail.com>
2019-07-05 08:32:05 +0100

    Small update

--- scripts.sh
+++ scripts.sh
@@ -6,10 +6,6 @@ sudo cp org.kde.krita.desktop /usr/share/applications/
 
 sudo cp soundcloud.desktop /usr/share/applications/
 
-sudo cp gaomon_2D.desktop /usr/share/applications/
-
-sudo cp gaomon_3D.desktop /usr/share/applications/
-
 sudo cp 70-wacom.conf /usr/share/X11/xorg.conf.d/
 
 sudo cp 10-evdev.conf /usr/share/X11/xorg.conf.d/
