bc0b5094d6390e3fd6d3e1fa5629568801730434
veryprofessionaldodo <pedroreis.ad@gmail.com>
2019-07-23 09:31:02 +0100

    Small fixes to volume control

--- Scripts/backlight.sh
+++ Scripts/backlight.sh
@@ -5,9 +5,10 @@ backlight="$(xbacklight)"
 if [[ "$@" = "-dec 10" ]] && [ ${backlight%.*} -lt 11 ] && [ ${backlight%.*} -gt 1 ];
 then
     xbacklight -set 1
-        
+    dunstify "Backlight: ${backlight%.*}"    
 else
   xbacklight "$@"
+  dunstify "Backlight: ${backlight%.*}"
 fi
 
 pkill -RTMIN+2 i3blocks
