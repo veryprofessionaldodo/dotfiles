b1f9d0e0bc1ef4633f1e7824c6f711083467d031
veryprofessionaldodo <pedroreis.ad@gmail.com>
2019-07-28 20:47:03 +0100

    Added small fix to Firefox

--- Scripts/backlight.sh
+++ Scripts/backlight.sh
@@ -5,10 +5,10 @@ backlight="$(xbacklight)"
 if [[ "$@" = "-dec 10" ]] && [ ${backlight%.*} -lt 11 ] && [ ${backlight%.*} -gt 1 ];
 then
     xbacklight -set 1
-    dunstify "Backlight: ${backlight%.*}"    
+    dunstify -r -5555 -u "Backlight: ${backlight%.*}"    
 else
   xbacklight "$@"
-  dunstify "Backlight: ${backlight%.*}"
+  dunstify -r 5555 -u "Backlight: ${backlight%.*}"
 fi
 
 pkill -RTMIN+2 i3blocks
