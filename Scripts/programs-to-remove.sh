88edba568588ce476df15e526a22a076fbeac497
veryprofessionaldodo <pedroreis.ad@protonmail.com>
2019-12-05 11:28:15 +0000

    Major refactor to polybar, now much cleaner

---
+++ Scripts/programs-to-remove.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+laptop="laptop"
+desktop="desktop"
+
+echo "\nCleaning up $1..."
+
+if [ $1 == $laptop ]
+then
+	yay -Sc
+	yay -Rs texlive-most
+elif [ $1 == $desktop ]
+then
+	yay -Sc 
+	yay -Rs texlive-most
+fi
+
+echo "All cleaned up!\n"
+
