24c565f9d00edcf657f878807f103571d895da8c
veryprofessionaldodo <pedroreis.ad@protonmail.com>
2019-12-29 14:11:55 +0000

    SOU UM DEUS

--- Scripts/toggleMonitorRate.py
+++ Scripts/toggleMonitorRate.py
@@ -5,7 +5,8 @@ import time
 
 rates = subprocess.run('xrandr', stdout=subprocess.PIPE).stdout.decode().rstrip()
 
-settings = [line for line in rates.split('\n') if "74.97" in line]
+# Number of settings equates to number of active displays
+settings = [line for line in rates.split('\n') if "*" in line]
 
 toggled = "60.00"
 # Was 60, toggle to 75
@@ -14,10 +15,15 @@ if "60.00*" in settings[0]:
 
 print(settings)
 
-command = "xrandr --output DisplayPort-0 --primary --rate " + toggled + " --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1920x630 --rotate normal"
+# Two monitors active
+if len(settings) == 2:
+  command = "xrandr --output DisplayPort-0 --primary --rate " + toggled + " --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1920x630 --rotate normal"
+else:
+  command = "xrandr --output DisplayPort-0 --primary --rate " + toggled + " --mode 1920x1080 --pos 0x0 --rotate normal"
 
 os.system(command)
 
 notification = "notify-send \"Toggled to: "+ toggled + "\"" 
 os.system(notification)
+
 
